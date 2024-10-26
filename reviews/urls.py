ba1493a5efa2edff0e8aeabcefe9f982247cc0b7
Sachin <sachnaror@gmail.com>
2024-10-26 11:22:42 +0530

    Persistence paves the road to enlightenment

--- reviews/urls.py
+++ reviews/urls.py
@@ -4,9 +4,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.business_qr_view, name='business_qr'),  # Home or main view
+    path('', views.home, name='home'),  # Point root to the home view
     path('business/<int:id>/', views.business_qr_view, name='business_qr'),
     path('reviews/<int:id>/rate', views.review_stars, name='review_stars'),
     path('feedback/', views.feedback, name='feedback'),
 ]
-
